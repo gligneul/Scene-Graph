5186928d5d612a02de607ce7876369ab9db466c7
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2015-10-01 21:38:40 -0300

    Added Texture class dummy

---
+++ Texture.cpp
@@ -0,0 +1,23 @@
+/**
+ * PUC-Rio 2015.2
+ * INF1339 - Computação Gráfica Tridimensional
+ * Professor: Waldemar Celes
+ * Gabriel de Quadros Ligneul 1212560
+ * Trabalho - Projeto de Grafo de Cena
+ */
+
+#include "Texture.h"
+
+Texture::Texture(const std::string& path) {
+    (void)path;
+}
+
+Texture::~Texture() {
+}
+
+void Texture::Load() {
+}
+
+void Texture::Unload() {
+}
+
--- Texture.h
+++ Texture.h
@@ -6,3 +6,40 @@
  * Trabalho - Projeto de Grafo de Cena
  */
 
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <string>
+
+#include "Appearance.h"
+
+class Texture : public Appearance {
+public:
+    /**
+     * Constructor for an 2D texture
+     * Receives the path to the texture file (png only)
+     * Throws runtime_error if the file dimensions aren't base 2
+     */
+    Texture(const std::string& path);
+
+    /**
+     * Destructor
+     */
+    ~Texture();
+
+    /**
+     * Loads the appearance
+     */
+    void Load();
+
+    /**
+     * Unloads the appearance
+     */
+    void Unload();
+
+private:
+    unsigned int _id;
+};
+
+#endif
+
