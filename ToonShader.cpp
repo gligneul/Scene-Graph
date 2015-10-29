21aa6d07e2d53de0a548d9576fdc338e3dbb195e
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2015-10-29 10:13:55 -0200

    Added matrices to the vertex shader

--- ToonShader.cpp
+++ ToonShader.cpp
@@ -29,9 +29,20 @@ ToonShader::ToonShader() {
 
 void ToonShader::Load() {
     glUseProgram(program_);
+
+    float modelview[16];
+    glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
+    GLuint modelview_location = glGetUniformLocation(program_, "modelview");
+    glUniformMatrix4fv(modelview_location, 1, GL_FALSE, modelview);
+
+    float projection[16];
+    glGetFloatv(GL_PROJECTION_MATRIX, projection);
+    GLuint projection_location = glGetUniformLocation(program_, "projection");
+    glUniformMatrix4fv(projection_location, 1, GL_FALSE, projection);
 }
 
 void ToonShader::Unload() {
+    glUseProgram(0);
 }
 
 std::string ToonShader::ReadFile(const char *path) {
--- toonshader_vs.glsl
+++ toonshader_vs.glsl
@@ -10,7 +10,10 @@
 
 in vec3 vp;
 
+uniform mat4 modelview;
+uniform mat4 projection;
+
 void main () {
-    gl_Position = vec4(vp, 1.0);
+    gl_Position = projection * modelview * vec4(vp, 1.0);
 }
 
