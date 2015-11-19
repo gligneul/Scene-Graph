043e014a1ff06145fee205a895f430182b4f4436
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2015-11-19 19:07:05 -0200

    Added support for opengl 3

--- Light.cpp
+++ Light.cpp
@@ -79,7 +79,8 @@ void Light::EnableShadowMap(const glm::vec3& center, const glm::vec3& up,
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);  
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);  
-    glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, sm_texture_, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+            GL_TEXTURE_2D, sm_texture_, 0);
 
     GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
     if (status != GL_FRAMEBUFFER_COMPLETE)
